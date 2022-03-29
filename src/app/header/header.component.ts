040b7d7fd8aced7f11ef629bd014eeb34abceb59
Bruno C. Manso <mrbrunomanso@gmail.com>
2022-03-29 09:44:10 -0300

    @Input example can be find on seach using '// @Input Example'

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  // @Output Example:
+
   @Output() featureSelected = new EventEmitter<string>();
 
   collapsed = true;
@@ -17,7 +17,6 @@ export class HeaderComponent implements OnInit {
 
   onSelect(feature: string) {
     this.featureSelected.emit(feature); 
-    // the CHILD will @Output (emitt) to  any html PARENT that have (featuredSelected) event
-    // in this case will be send to app.component.html <app-header (featureSelected)="onNavigate($event)">
+
   }
 }
